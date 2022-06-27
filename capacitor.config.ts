e3d116a7482631efe83dee3ddf1316dc88939a2c
Necrobone <necrobone@hotmail.com>
2022-06-27 20:37:46 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'monster-builder-react',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
